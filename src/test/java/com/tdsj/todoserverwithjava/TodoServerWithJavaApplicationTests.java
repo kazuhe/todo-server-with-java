92e7fc5d140399dee0d8b3662e114296456b8b96
kazuhe <omc.ohara@gmail.com>
2022-11-14 00:24:59 +0900

    Setup with Spring Initializr

---
+++ src/test/java/com/tdsj/todoserverwithjava/TodoServerWithJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tdsj.todoserverwithjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoServerWithJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
